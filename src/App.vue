674d07066c7a17a84d7b94096c1cfbe41aa5be1c
Mehedi@1996 <mehedihasancmt2@gmail.com>
2023-08-13 22:22:26 +0600

    'inital'

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import {ref, onMounted, onBeforeMount, onUnmounted, onUpdated, onBeforeUnmount } from 'vue';
+
+let chart = null
+const dataset = [
+  300, 120, 151
+]
+
+const data = {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: dataset,
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+const config = {
+  type: 'doughnut',
+  data: data,
+};
+
+onMounted(()=> {
+  const ctx = document.getElementById('myChart');
+  chart = new Chart(ctx, config)
+});
+
+onBeforeUnmount(()=> {
+  chart.destroy()
+});
+
+
+
+</script>
+
+<template>
+    <div>
+  <canvas id="myChart"></canvas>
+</div>
+</template>
+
+<style scoped>
+
+</style>
